90b9f1abc89b8bac9dadfa61c259cebd3a960cf8
sayligadgil <sayligadgil06@gmail.com>
2025-04-24 14:33:49 +0530

    bubblesort.c

---
+++ bubblesort.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+void bubbleSort(int arr[], int n) {
+    int i, j, temp;
+    int swapped;
+
+    for (i = 0; i < n - 1; i++) {
+        swapped = 0; // flag to detect any swap
+
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // swap arr[j] and arr[j + 1]
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                swapped = 1;
+            }
+        }
+
+        // if no two elements were swapped in the inner loop, array is sorted
+        if (swapped == 0)
+            break;
+    }
+}
+
+void printArray(int arr[], int n) {
+    int i;
+    for (i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    printf("Original array:\n");
+    printArray(arr, n);
+
+    bubbleSort(arr, n);
+
+    printf("Sorted array:\n");
+    printArray(arr, n);
+
+    return 0;
+}
+
